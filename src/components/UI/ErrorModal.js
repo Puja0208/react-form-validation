0c48389d76a352bc2855718d9ee8c6998a4a2f49
puja.rani <puja.rani@ril.com>
2022-02-27 06:37:00 +0530

    feat: Create react portal for overlay & backdrop

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,13 +1,16 @@
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
 function ErrorModal(props) {
-  //   const clickHandler = () => {
-  //     props.onDialogClose();
-  //   };
-  return (
-    <>
+  const Backdrop = (props) => {
+    return (
       <div onClick={props.onDialogClose} className={styles.backdrop}></div>
+    );
+  };
+
+  const ModalOverlay = (props) => {
+    return (
       <Card className={styles.modal}>
         <header className={styles.header}>
           <h2>{props.title}</h2>
@@ -19,6 +22,23 @@ function ErrorModal(props) {
           <Button onClick={props.onDialogClose}>Okay</Button>
         </footer>
       </Card>
+    );
+  };
+
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onDialogClose={props.onDialogClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          onDialogClose={props.onDialogClose}
+          title={props.title}
+          message={props.message}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 }
